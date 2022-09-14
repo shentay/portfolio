0f53562603ff6e0f1b8c36a655850761cfcbc696
Yuan Shen Tay <shentay@terpmail.umd.edu>
2022-09-14 15:56:10 -0400

    'initial'

--- src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Contact() {
-    return (
-        <section id="contact" className="relative">
-            <p className="leading relaxed">240-825-5973</p>
-        </section>
-    )
-}
