c526742d1199cf54bd896bcefe0b816b9959a143
Yuan Shen Tay <shentay@terpmail.umd.edu>
2023-01-10 15:48:25 -0500

    have not edit for mobile

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ window.onload = function() {
     // change active page
     let fileName = location.pathname.split("/").slice(-1)[0]
     let items = document.getElementsByClassName('navbar-inactive');
-    // set footer to be correct position
-    let footer = document.getElementsByClassName('footer');
     switch (fileName) {
         case 'index.html':
             items[0].className = 'navbar-active';
-            console.log(footer)
-            footer[0].id = 'footer-about';
             break
         case 'skills.html':
             items[1].className = 'navbar-active'
